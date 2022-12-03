c41f95525e5185cee0719fdf33accc7ba8cfe678
domzic <domas.zicevicius@gmail.com>
2022-12-03 13:49:13 +0200

    Setup typescript

--- src/2022/01/index.ts
+++ src/2022/01/index.ts
@@ -1,12 +1,15 @@
-const getInput = require('../data');
+import getInput from "../data";
+
+function sum(array: number[]): number {
+    return array.reduce((acc, current) => acc + current, 0);
+}
 
 (async function() {
     const input = await getInput('01');
-    const sum = array => array.reduce((acc, current) => acc + parseInt(current), 0);
 
     const totals = input
         .split('\n\n')
-        .map(elf => sum(elf.split('\n')))
+        .map((elf: string) => sum(elf.split('\n').map(n => parseInt(n))))
         .sort((a, b) => b - a);
 
     console.log('Totals: ', totals[0]);
---
+++ src/2022/data/index.ts
@@ -0,0 +1,13 @@
+import fs from 'fs';
+
+const getInput = (day: string) => new Promise<string>((resolve, reject) => {
+    fs.readFile(`${__dirname}/${day}.txt`, {}, (error, data) => {
+        if (error) {
+            return reject(new Error('Input file for specified date was not found.'));
+        }
+
+        return resolve(data.toString());
+    });
+});
+
+export default getInput;
