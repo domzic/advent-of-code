a98215ddfa8b861e016cdaa0c119b0e2131b461e
domzic <domas.zicevicius@gmail.com>
2022-12-03 14:33:48 +0200

    Setup prettier & eslint

--- src/2022/01/index.ts
+++ src/2022/01/index.ts
@@ -1,17 +1,17 @@
-import getInput from "../data";
+import getInput from '../data';
 
 function sum(array: number[]): number {
     return array.reduce((acc, current) => acc + current, 0);
 }
 
-(async function() {
+(async function () {
     const input = await getInput('01');
 
     const totals = input
         .split('\n\n')
-        .map((elf: string) => sum(elf.split('\n').map(n => parseInt(n))))
+        .map((elf: string) => sum(elf.split('\n').map((n) => parseInt(n))))
         .sort((a, b) => b - a);
 
     console.log('Totals: ', totals[0]);
-    console.log('Top 3: ', sum(totals.slice(0, 3)))
+    console.log('Top 3: ', sum(totals.slice(0, 3)));
 })();
