e8fe0bb649bad4664e90e9242a2319f439038e58
domzic <domas.zicevicius@gmail.com>
2023-12-01 08:28:39 +0200

    Adjust runner for 2023 year, make input module dynamic

---
+++ src/2023/01/index.ts
@@ -0,0 +1,3 @@
+export default async function (input: string) {
+    console.log(input);
+}
--- src/runner/index.ts
+++ src/runner/index.ts
@@ -1,23 +1,25 @@
 import { program } from 'commander';
-import getInput from '../2022/data';
 
 program
     .version('1.0.0')
     .description('CLI to run advent of code scripts')
     .option('-d, --day <day>', 'day', '01')
-    .option('-y, --year <year>', 'year', '2022')
+    .option('-y, --year <year>', 'year', '2023')
     .option('-f, --file <file>', 'file name', 'index.ts')
     .parse(process.argv);
 
 const { year, day, file } = program.opts();
+const inputPath = `${process.cwd()}/src/${year}/data`;
 const path = `${process.cwd()}/src/${year}/${day}/${file}`;
 
-import(path)
-    .then(async (dynamicModule) => {
-        console.info('\x1b[36m%s\x1b[0m', path + ':');
-        const input = await getInput(day);
-        dynamicModule.default(input);
-    })
-    .catch((err) => {
-        console.log('\x1b[31m', err);
-    });
+import(inputPath).then(async (dynamicInputModule) => {
+    import(path)
+        .then(async (dynamicModule) => {
+            console.info('\x1b[36m%s\x1b[0m', path + ':');
+            const input = await dynamicInputModule.default(day);
+            dynamicModule.default(input);
+        })
+        .catch((err) => {
+            console.log('\x1b[31m', err);
+        });
+});
