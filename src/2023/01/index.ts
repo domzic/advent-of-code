a2c0fdf16888062b226ed09ba11239ad52f24fc2
domzic <domas.zicevicius@gmail.com>
2023-12-01 09:52:57 +0200

    day 1, part 2

--- src/2023/01/index.ts
+++ src/2023/01/index.ts
@@ -1,16 +1,43 @@
 export default async function (input: string) {
     const lines = input.split('\n');
-    const numbers: number[] = [];
-    lines.forEach((line) => {
-        const numbersFound = [...line].filter(isNumber);
-        numbers.push(
-            +(numbersFound[0] + numbersFound[numbersFound.length - 1])
-        );
-    });
-    const sum = numbers.reduce((num, sum) => sum + num);
-    console.log({ numbers, sum });
+    const sum = lines.reduce<number>(
+        (sum, line) => sum + extractNumber(line),
+        0
+    );
+    console.log({ sum });
 }
 
-function isNumber(char: string) {
-    return char.length === 1 && char.match(/[0-9]/);
+function extractNumber(line: string): number {
+    const digitsRegex =
+        /(one|two|three|four|five|six|seven|eight|nine|zero|\d)/gi;
+
+    // "twone" => "twoone" that both words would be matched by regex
+    line = line.replaceAll(digitsRegex, (match) => {
+        if (match.length > 1) {
+            return match + match[match.length - 1];
+        }
+        return match;
+    });
+
+    const digits = (line.match(digitsRegex) || []).map((digit) =>
+        isNaN(+digit) ? WordToNumber[digit.toLowerCase()] : +digit
+    );
+
+    if (digits.length > 0) {
+        return digits[0] * 10 + digits[digits.length - 1];
+    } else {
+        return 0;
+    }
 }
+
+const WordToNumber: Record<string, number> = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+};
